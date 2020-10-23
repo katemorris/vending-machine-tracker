295ee460e33a5a550d04b44008d496ed0ce96905
Kate Tester <katemorris@gmail.com>
2020-10-22 20:54:09 -0600

    Adds test for average snack price method.

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks) }
   end
+
+  describe 'instance variables' do
+    it ".average_snack_price" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      doritos = Snack.create(name: 'Doritos', price: 2.50)
+      cookies = Snack.create(name: "Grandma's Cookies", price: 2.00)
+      MachineSnack.create(snack: doritos, machine: dons)
+      MachineSnack.create(snack: cookies, machine: dons)
+
+      expect(dons.average_snack_price).to eq(2.25)
+    end
+  end
 end
